0e7dbf2eeecbdf2dec282fb72614b07e9ccfd06d
jesus <jesus@LAPTOP-JFGE>
2021-02-27 10:55:12 -0600

    JAMO|Se reemplaza pdf para descarga desde post

--- src/main/java/controller/reportePDF.java
+++ src/main/java/controller/reportePDF.java
@@ -1,92 +1,213 @@
 package controller;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
-
-import com.itextpdf.text.DocumentException;
+import java.util.HashMap;
 import com.itextpdf.text.pdf.AcroFields;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Servlet implementation class reportePDF
  */
 public class reportePDF extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * Default constructor. 
-     */
-    public reportePDF() {
-        // TODO Auto-generated constructor stub
-    }
+	public static HashMap<String, String> etiquetas;
+	static {
+		etiquetas= new HashMap<String, String>();
+		etiquetas.put("name", "nombre");
+		etiquetas.put("product", "producto");
+	}
+	
+	
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Default constructor.
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			InputStream is = getClass().getResourceAsStream("/reports/etiquetas.pdf");
-			PdfReader reader = new PdfReader(is, null);
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			PdfStamper stamper = new PdfStamper(reader, baos);
-			AcroFields fields = stamper.getAcroFields();
-			fields.setField("atn_dependencia","JESUS MOHEDANO");
-			stamper.setFormFlattening(true);
-			stamper.close();
-			OutputStream os = response.getOutputStream();
-			baos.writeTo(os);
-			os.flush();
-			} catch (DocumentException de) {
-			throw new IOException(de.getMessage());
+	public reportePDF() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {	
+		System.out.println("INICIO");
+		//Revisar que llegue el objeto json con client
+	try {
+		
+		String jsonStr = getBody(request);
+		System.out.println(jsonStr);
+		/*JSONObject jsonObject = new JSONObject(jsonStr);
+		Map<String, Object> map = jsonObject.toMap();
+		HashMap<String, HashMap<String, String>> valorEtiquetas = new HashMap<String, HashMap<String, String>>();
+		HashMap<String, String> etiquetasMap = null;
+		ArrayList etiquetasList = null;
+		
+			if(!entry.getKey().equals("product") && !entry.getKey().equals("references")) {
+				valorEtiquetas.put(entry.getKey(), (HashMap<String, String>) entry.getValue());
+			}else {
+				
+				etiquetasList = (ArrayList) entry.getValue();
+				Integer i = 0;
+				Iterator it = etiquetasList.iterator();
+				while(it.hasNext()) {
+					valorEtiquetas.put(entry.getKey().concat(i.toString()), (HashMap<String, String>) it.next());
+					i = i+1;;
+				}
 			}
-		response.setContentType("application/pdf");
-		response.setHeader("Cache-control","must-revalidate, post-check=0, pre-check=0");
-		/*String idCliente = request.getParameter("idCliente");
-		String urlString = "https://reqres.in/api/users/";
-		if(idCliente.trim() == null ||idCliente.trim() == "") {
-			urlString=urlString.concat("");
-		}else {
-			urlString=urlString.concat(idCliente);
-		}   
-	    
-		InputStream is = getClass().getResourceAsStream("/reports/FormatoPreCaptura.jrxml");
-	    Map parameters = new HashMap();
-	      parameters.put("path", "/images/precaptura.jpg");
-	      parameters.put(JsonQueryExecuterFactory.JSON_SOURCE, urlString);
+		}*/
+		InputStream is = getClass().getResourceAsStream("/reports/AtnContract.pdf");
+		PdfReader reader = new PdfReader(is, null);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PdfStamper stamper = new PdfStamper(reader, baos);
+		AcroFields fields = stamper.getAcroFields();
+		fields.setField("nombre1", "JESUS ANTONIO MOHEDANO ORTEGA");
 
+		stamper.setFormFlattening(true);
+		stamper.close();
 		try {
-			 JasperReport report = JasperCompileManager.compileReport(is);
-			 JasperPrint jasperPrint = JasperFillManager.fillReport(report, parameters);
-			 byte[] pdfByteArray = JasperExportManager.exportReportToPdf(jasperPrint);
-			 response.setContentType("application/pdf");
-		      response.setContentLength(pdfByteArray.length);
-		      ServletOutputStream ouputStream = response.getOutputStream();
-		      ouputStream.write(pdfByteArray, 0, pdfByteArray.length);
-		      ouputStream.flush();
-		      ouputStream.close();
-		} catch (JRException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			response.setHeader("Expires", "0");
+			 response.setHeader("Cache-Control",
+			 "must-revalidate, post-check=0, pre-check=0");
+			 response.setHeader("Pragma", "public");
+			 response.setContentType(
+					 "application/vnd.adobe.pdf");
+					 response.setHeader("Content-Disposition",
+					 "attachment; filename=\"contrato.pdf\"");
+			 response.setContentLength(baos.size());
+			 response.addHeader("Access-Control-Allow-Origin", "true");
+			 OutputStream os = response.getOutputStream();
+			 System.out.println("escritura");
+			 baos.writeTo(os);
+			 os.flush();
+			 System.out.println("confirmar");
+			 os.close(); 
+		}catch(Exception de) {
+			System.out.println("error escritura");
+			System.out.println(de);
 		}
-*/
-	    /*System.out.println(String.format("Status: %d, JSON Payload: %s", status, json));
-		response.getWriter().append("Served at: ").append(json);*/
+	} catch (Exception de) {
+		System.out.println(de);
+		//throw new IOException(de.getMessage());
 	}
+	System.out.println("FIN");
+	
+}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {	
+		System.out.println("INICIO");
+		//Revisar que llegue el objeto json con client
+	try {
+		
+		String jsonStr = getBody(request);
+		System.out.println(jsonStr);
+		/*JSONObject jsonObject = new JSONObject(jsonStr);
+		Map<String, Object> map = jsonObject.toMap();
+		HashMap<String, HashMap<String, String>> valorEtiquetas = new HashMap<String, HashMap<String, String>>();
+		HashMap<String, String> etiquetasMap = null;
+		ArrayList etiquetasList = null;
+		
+			if(!entry.getKey().equals("product") && !entry.getKey().equals("references")) {
+				valorEtiquetas.put(entry.getKey(), (HashMap<String, String>) entry.getValue());
+			}else {
+				
+				etiquetasList = (ArrayList) entry.getValue();
+				Integer i = 0;
+				Iterator it = etiquetasList.iterator();
+				while(it.hasNext()) {
+					valorEtiquetas.put(entry.getKey().concat(i.toString()), (HashMap<String, String>) it.next());
+					i = i+1;;
+				}
+			}
+		}*/
+		InputStream is = getClass().getResourceAsStream("/reports/AtnContract.pdf");
+		PdfReader reader = new PdfReader(is, null);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PdfStamper stamper = new PdfStamper(reader, baos);
+		AcroFields fields = stamper.getAcroFields();
+		fields.setField("nombre1", "JESUS ANTONIO MOHEDANO ORTEGA");
 
+		stamper.setFormFlattening(true);
+		stamper.close();
+		try {
+			response.setHeader("Expires", "0");
+			 response.setHeader("Cache-Control",
+			 "must-revalidate, post-check=0, pre-check=0");
+			 response.setHeader("Pragma", "public");
+			 response.setContentType(
+					 "application/vnd.adobe.fdf");
+					 response.setHeader("Content-Disposition",
+					 "attachment; filename=\"subscribe.fdf\"");
+			 response.setContentLength(baos.size());
+			 response.addHeader("Access-Control-Allow-Origin", "true");
+			 OutputStream os = response.getOutputStream();
+			 System.out.println("escritura");
+			 baos.writeTo(os);
+			 os.flush();
+			 System.out.println("confirmar");
+			 os.close(); 
+		}catch(Exception de) {
+			System.out.println("error escritura");
+			System.out.println(de);
+		}
+	} catch (Exception de) {
+		System.out.println(de);
+		//throw new IOException(de.getMessage());
+	}
+	System.out.println("FIN");
+	
+}
+	public static String getBody(HttpServletRequest request) throws IOException {
+
+	    String body = null;
+	    StringBuilder stringBuilder = new StringBuilder();
+	    BufferedReader bufferedReader = null;
+
+	    try {
+	        InputStream inputStream = request.getInputStream();
+	        if (inputStream != null) {
+	            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+	            char[] charBuffer = new char[128];
+	            int bytesRead = -1;
+	            while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+	                stringBuilder.append(charBuffer, 0, bytesRead);
+	            }
+	        } else {
+	            stringBuilder.append("");
+	        }
+	    } catch (IOException ex) {
+	        throw ex;
+	    } finally {
+	        if (bufferedReader != null) {
+	            try {
+	                bufferedReader.close();
+	            } catch (IOException ex) {
+	                throw ex;
+	            }
+	        }
+	    }
+
+	    body = stringBuilder.toString();
+	    return body;
+	}
 }
